6f09dd163d889e992100da7b1a9e7ad8b813d742
James-Fraser1 <jdfraser2022@gmail.com>
2022-08-03 13:01:35 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require('express');
 const PORT = process.env.PORT || 3001;
+const express = require('express');
 const app = express();
 const { animals } = require('./data/animals.json')
-
-
-
-
-
-
 
 
 
@@ -63,5 +57,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
-});
+    console.log(`API server now on port ${PORT}!`);
+  });
